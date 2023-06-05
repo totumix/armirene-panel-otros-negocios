cfe03f65f45a972bec949be3215c2f87105b7a98
Sebastian chavez <juans.chavez@farmatodo.com>
2023-06-05 13:25:16 -0500

    componente mapa individualizado

---
+++ src/app/shared/components/map/map.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { selectDataMapInterface } from '../../interfaces/select-data-map.type';
+import { ViewportMap } from '../view-port-map/view-port-map';
+import { fromFetch } from 'rxjs/fetch';
+import { from, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+  standalone: true,
+
+})
+export class MapComponent implements OnInit, OnDestroy {
+  map = ViewportMap.getInstance();
+  selectedData: selectDataMapInterface;
+  onGPS = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.map.callbackDrop = (data) => {
+      this.selectedData.lat = data?.lat;
+      this.selectedData.lng = data?.lng;
+      this.resolveCoordinatesToAddress(this.selectedData);
+    }
+    setTimeout(() => { this.getCurrentLocation(); }, 5);
+    // this.map.init({ lat: 4.68234, lng: -74.043835 })
+  }
+
+  getCurrentLocation() {
+    //console.clear();
+    navigator.geolocation.getCurrentPosition((position) => {
+      if (!this.selectedData?.address || this.selectedData?.address?.length < 1) {
+        this.onGPS = true;
+        this.selectedData = { ...this.selectedData, lat: position.coords.latitude, lng: position.coords.longitude }
+        this.map.init({ lat: position.coords.latitude, lng: position.coords.longitude })
+        // this.selectedData.lat = position.coords.latitude;
+        // this.selectedData.lng = position.coords.longitude;
+        // this.setPositionMarker();
+        this.resolveCoordinatesToAddress(this.selectedData);
+      }
+    });
+  }
+
+  resolveCoordinatesToAddress({ lat, lng }) {
+    let urlInver = 'https://nominatim.openstreetmap.org/reverse?format=jsonv2&addressdetails=0&zoom=40&lat=';
+    urlInver += `&lat=${lat}&lon=${lng}`;
+    fromFetch(urlInver)
+      .pipe(switchMap((r: any) => from(r.json())))
+      .subscribe((json) => {
+        console.log(json);
+      }, (error) => {
+        console.log(error);
+      });
+  }
+
+  ngOnDestroy() {
+    this.onGPS = false;
+  }
+}
